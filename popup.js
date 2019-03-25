19730ab6c32d77240e25f2407f764b5d19200014
tanimotohikari <tanimoto.hikari@gmail.com>
2019-03-25 22:57:40 +0900

    update:master localStorage

--- popup.js
+++ popup.js
@@ -1,20 +1,29 @@
 'use strict'
 $(function() {
 
-  $('.row').on('click', function() {
-   $('input[type=checkbox]', this).prop('checked', function(i, checked){
-      console.log(i);
-      return !checked
-   })
-
-  // チェックされた時のstyleを付与する
-  if($('input[type=checkbox]', this).prop('checked'))
-    $(this).addClass('selected');
-  else
-    $(this).removeClass('selected');
-  });
-
   // listの数
-  console.log($('.check-list > li').length);
+  var listCount = $('.check-list > li').length;
+  $('#js-list-count').html(listCount)
 
+  $('input').click(function() {
+    // for (var i = 0; i < listCount; i++) {
+    //   localStorage.setItem('vwoCheck-' + i, check);
+    // }
+    var check = $(this).prop('checked');
+    var index = $('input').index(this);
+    localStorage.setItem('vwoCheck-' + index, check);
+  })
+
+  function getStorageData() {
+    for(var i = 0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      if(key.match('vwoCheck'))
+      console.log(key);
+      // if(key.match(/check/)) {
+      //   console.log('hoge');
+      // }
+    }
+  }
+
+  getStorageData();
 });
