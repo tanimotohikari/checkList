ea3ff7ad402640d33f78cfcdb3fdc6a35e4d6b4e
tanimotohikari <tanimoto.hikari@gmail.com>
2019-03-27 09:16:56 +0900

    update:master クリアボタンの実装

--- popup.js
+++ popup.js
@@ -1,34 +1,44 @@
 'use strict'
 $(function() {
 
+  var $btn = $('#js-btn-clear');
+  var ids = [];
+
   // listの数
   var listCount = $('.check-list > li').length;
   $('#js-list-count').html(listCount)
 
-  $('input').click(function() {
-    // for (var i = 0; i < listCount; i++) {
-    //   localStorage.setItem('vwoCheck-' + i, check);
-    // }
+  // チェックボックスをクリックした時
+  $('input').on('click', function() {
     var check = $(this).prop('checked');
     var index = $('input').index(this);
     localStorage.setItem('vwoCheck-' + index, check);
   })
+
+  // クリアボタンを押した時
+  $btn.on('click', clearData);
 
   function getStorageData() {
     for(var i = 0; i < localStorage.length; i++) {
       var key = localStorage.key(i);
       if(key.match('vwoCheck')) {
         var index = key.split('-');
+        ids.push('#checkbox' + index[1]);
         var id = '#checkbox' + index[1];
-        console.log(id);
         var boolean = localStorage.getItem(key);
         $(id).attr('checked', boolean);
       }
-      // if(key.match(/check/)) {
-      //   console.log('hoge');
-      // }
+    }
+  }
+
+  function clearData() {
+    localStorage.clear();
+    alert('チェックされたデータはクリアされました')
+    for (var i = 0; i < ids.length; i++) {
+      $(ids[i]).attr('checked', false);
     }
   }
 
   getStorageData();
+  console.log(ids);
 });
